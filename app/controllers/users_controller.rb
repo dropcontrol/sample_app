d22480ff1c2b69d914c883e904fc9c071b84d05a
hiroshi yamato <yamato@allianceport.jp>
2013-11-24 17:24:13 +0900

    exam

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
+  before_action :not_allow_to_signed_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -12,18 +13,18 @@ class UsersController < ApplicationController
 	end
 
   def new
-  	@user = User.new
+    	@user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-    	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
-    end
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end      
   end
 
   def edit
@@ -39,9 +40,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    # @user = User.find(params[:id])
+    # unless current_user?(@ueer)
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    # end
   end
 
   private
@@ -51,11 +55,6 @@ class UsersController < ApplicationController
     end
 
     # Before actions
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
@@ -63,5 +62,9 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def not_allow_to_signed_in_user
+      redirect_to(root_path) if signed_in?
     end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -82,7 +82,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -136,6 +136,37 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+    end
+
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+  end
+
+  describe "permited action for signed-in user" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user, no_capybara: true }
+
+    describe "new" do
+      describe "submitting a GET request to the Users#new action" do
+        before { get new_user_path(user) }
+        specify { expect(response.body).not_to match(full_title('Sign up')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+    describe "create" do
+      describe "submitting a GET request to the Users#new action" do
+        before { post users_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
     end
   end
 end
